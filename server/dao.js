e33bc5777555fc3f0867fade42069d9cf208e5ce
Francesco Policastro s281705 <s281705@studenti.polito.it>
2021-07-01 18:09:34 +0200

    Added API's backend (added dao.js, server.js, api.http) + updated database table

---
+++ server/dao.js
@@ -0,0 +1,197 @@
+const sqlite = require('sqlite3');
+const bcrypt = require('bcrypt');
+
+// open the db
+const db = new sqlite.Database('db.sqlite', (err) => {
+  if (err) throw err;
+});
+
+//get all (public & protected) memes 
+exports.getAllMemes = () => new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM memes';
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        const memes = rows.map((m) => (
+            {
+                id: m.id,
+                id_template: m.id_template,
+                title: m.title,
+                text0: m.text0,
+                text1: m.text1,
+                text2: m.text2,
+                text3: m.text3,
+                color: m.color,
+                font: m.font,
+                size: m.size,
+                protected: m.protected,
+                image: m.image,
+                user: m.user,
+            }));
+
+        resolve(memes);
+    });
+});
+
+//get all public memes
+exports.getPublicMemes = () => new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM memes WHERE protected = 0';
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        const memes = rows.map((m) => (
+            {
+                id: m.id,
+                id_template: m.id_template,
+                title: m.title,
+                text0: m.text0,
+                text1: m.text1,
+                text2: m.text2,
+                text3: m.text3,
+                color: m.color,
+                font: m.font,
+                size: m.size,
+                protected: m.protected,
+                image: m.image,
+                user: m.user,
+            }));
+
+        resolve(memes);
+    });
+});
+//get all the user's memes
+exports.getUserMemes = (user) => new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM memes WHERE user = ?';
+    db.all(sql, [user], (err, rows) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        const memes = rows.map((m) => (
+            {
+                id: m.id,
+                id_template: m.id_template,
+                title: m.title,
+                text0: m.text0,
+                text1: m.text1,
+                text2: m.text2,
+                text3: m.text3,
+                color: m.color,
+                font: m.font,
+                size: m.size,
+                protected: m.protected,
+                image: m.image,
+                user: m.user,
+            }));
+
+        resolve(memes);
+    });
+});
+
+//add a meme
+exports.addMeme = (userId, meme) => new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO memes (id, id_template, title, text0, text1, text2, text3, color, font, size, protected, image, user) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)';
+    db.run(sql, [
+        this.lastID,
+        meme.id_template,
+        meme.title,
+        meme.text0,
+        meme.text1,
+        meme.text2,
+        meme.text3,
+        meme.color,
+        meme.font,
+        meme.size,
+        meme.protected,
+        meme.image,
+        userId],
+        (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(this.lastID);
+        });
+});
+
+//get a meme (copy)
+exports.getMeme = (memeId) => new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM memes WHERE id = ?';
+    db.get(sql, [memeId], (err, row) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        const meme =
+        {
+            id: row.id,
+            id_template: row.id_template,
+            title: row.title,
+            text0: row.text0,
+            text1: row.text1,
+            text2: row.text2,
+            text3: row.text3,
+            color: row.color,
+            font: row.font,
+            size: row.size,
+            protected: row.protected,
+            image: row.image,
+            user: row.user
+        };
+        resolve(meme);
+    });
+});
+
+//delete a meme
+exports.deleteMeme = (userId, memeId) => new Promise((resolve, reject) => {
+    const sql = 'DELETE FROM memes WHERE user = ? AND id = ?';
+    db.run(sql, [userId, memeId], (err) => {
+        if (err) {
+            reject(err);
+        } else resolve(null);
+    });
+});
+
+// get user
+exports.getUser = (email, password) => new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    db.get(sql, [email], (err, row) => {
+        if (err) {
+            reject(err);
+        } else if (row) {
+            const user = { id: row.id, username: row.email, name: row.name };
+            bcrypt.compare(password, row.password).then((result) => {
+                if (result) {
+                    resolve(user);
+                } else {
+                    resolve(false);
+                }
+            });
+        } else {
+            resolve(false);
+        }
+    });
+});
+  
+// get user by id
+exports.getUserById = (id) => new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM users WHERE id = ?';
+    db.get(sql, [id], (err, row) => {
+        if (err) {
+            reject(err);
+        } else if (row) {
+            const user = { id: row.id, username: row.email, name: row.name };
+            resolve(user);
+        } else {
+            resolve({ err: 'User not found.' });
+        }
+    });
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,146 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan'); // logging middleware
+const session = require('express-session'); // session middleware
+const { check, validationResult } = require('express-validator'); // validation middleware
+const passport = require('passport');
+const passportLocal = require('passport-local');
 
-// init express
-const app = new express();
+const dao = require('./dao');
+
+passport.use(new passportLocal.Strategy((username, password, done) => {
+  dao.getUser(username, password).then((user) => {
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false, { message: 'Wrong username or password' });
+    }
+  }).catch((err) => {
+    done(err);
+  });
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  dao.getUserById(id).then((user) => {
+    done(null, user);
+  }).catch((err) => {
+    done(null, err);
+  });
+});
+
+const app = express();
 const port = 3001;
+
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({ message: 'not authenticated' });
+};
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(session({
+  secret: 'qzwsxedcrfvtgbyhnujmikol',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Retrieve ALL created memes (both PUBLIC and PROTECTED)
+app.get('/api/memes', isLoggedIn, (req, res) => {
+  dao.getAllMemes()
+    .then((memes) => { res.json(memes); })
+    .catch((error) => { res.status(500).json(error); });
+});
+
+// Retrieve all PUBLIC memes 
+app.get('/api/memes/public', (req, res) => {
+  dao.getPublicMemes()
+    .then((memes) => { res.json(memes); })
+    .catch((error) => { res.status(500).json(error); });
+});
+
+// Retrieve all USER's memes
+app.get('/api/memes/user', isLoggedIn, (req, res) => {
+  const userId = req.user.id;
+  dao.getUserMemes(userId)
+    .then((memes) => { res.json(memes); })
+    .catch((error) => { res.status(500).json(error); });
+});
+
+// Retrieve a meme (copy)
+app.get('/api/memes/copy/:id', isLoggedIn, (req, res) => {
+  const memeId = req.params.id;
+  dao.getMeme(memeId)
+    .then((memes) => { res.json(memes); })
+    .catch((error) => { res.status(500).json(error); });
+});
+
+// Create a new meme
+app.post('/api/memes', isLoggedIn, async (req, res) => {
+  const userId = req.user.id;
+  const meme = {
+    id_template: m.id_template,
+    title: req.body.title,
+    text0: req.body.text0,
+    text1: req.body.text1,
+    text2: req.body.text2,
+    text3: req.body.text3,
+    color: req.body.color,
+    font: req.body.font,
+    size: req.body.size,
+    protected: req.body.protected,
+    image: req.body.image,
+  };
+
+  try {
+    await dao.addMeme(userId, meme);
+    res.status(201).end();
+  } catch (err) {
+    res.status(503).json({ error: `Database error during the creation of meme: ${meme.title}.` });
+  }
+});
+
+// delete a meme
+app.deleteMeme('/api/memes/:id', isLoggedIn, async (req, res) => {
+  const userId = req.user.id;
+  const memeId = req.params.id;
+
+  try {
+    await dao.deleteMeme(userId, memeId);
+    res.status(204).end();
+  } catch (err) {
+    res.status(503).json({ error: `Database error during the deletion of meme: ${req.params.id}.` });
+  }
+});
+
+/**
+ * Users APIs
+ */
+
+/* POST /api/sessions */
+app.post('/api/sessions', passport.authenticate('local'), (req, res) => {
+  res.json(req.user);
+});
+
+/* DELETE /api/sessions/current */
+app.delete('/api/sessions/current', isLoggedIn, (req, res) => {
+  req.logout();
+  res.end();
+});
+
+/* GET /api/sessions/current */
+app.get('/api/sessions/current', isLoggedIn, (req, res) => {
+  res.json(req.user);
+});
 
 // activate the server
 app.listen(port, () => {
